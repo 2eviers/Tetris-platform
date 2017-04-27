8b764f4fee5bda467739ac95047d6614cd89ff31
Pierre Burgmann <pierre.burgmann@gmail.com>
2017-04-27 17:47:25 +0200

    Update on fixed transform based on bounding box
     Changes to be committed:
    	modified:   Assets/Prefabs/Tetris Blocks/IBlock.prefab
    	new file:   Assets/Prefabs/Tetris Blocks/IBlockPivot.prefab
    	new file:   Assets/Prefabs/Tetris Blocks/IBlockPivot.prefab.meta
    	modified:   Assets/Prefabs/Tetris Blocks/JBlock.prefab
    	new file:   Assets/Prefabs/Tetris Blocks/JBlockPivot.prefab
    	new file:   Assets/Prefabs/Tetris Blocks/JBlockPivot.prefab.meta
    	modified:   Assets/Prefabs/Tetris Blocks/LBlock.prefab
    	new file:   Assets/Prefabs/Tetris Blocks/LBlockPivot.prefab
    	new file:   Assets/Prefabs/Tetris Blocks/LBlockPivot.prefab.meta
    	modified:   Assets/Prefabs/Tetris Blocks/OBlock.prefab
    	new file:   Assets/Prefabs/Tetris Blocks/OBlockPivot.prefab
    	new file:   Assets/Prefabs/Tetris Blocks/OBlockPivot.prefab.meta
    	modified:   Assets/Prefabs/Tetris Blocks/SBlock.prefab
    	new file:   Assets/Prefabs/Tetris Blocks/SBlockPivot.prefab
    	new file:   Assets/Prefabs/Tetris Blocks/SBlockPivot.prefab.meta
    	modified:   Assets/Prefabs/Tetris Blocks/TBlock.prefab
    	new file:   Assets/Prefabs/Tetris Blocks/TBlockPivot.prefab
    	new file:   Assets/Prefabs/Tetris Blocks/TBlockPivot.prefab.meta
    	modified:   Assets/Prefabs/Tetris Blocks/ZBlock.prefab
    	new file:   Assets/Prefabs/Tetris Blocks/ZBlockPivot.prefab
    	new file:   Assets/Prefabs/Tetris Blocks/ZBlockPivot.prefab.meta
    	modified:   Assets/Scenes/TetrisTest.unity
    	modified:   Assets/Scripts/Blocks/BlocksMove.cs
    	new file:   Assets/Scripts/Blocks/PivotBlocksMove.cs
    	new file:   Assets/Scripts/Blocks/PivotBlocksMove.cs.meta

--- Assets/Scripts/Blocks/BlocksMove.cs
+++ Assets/Scripts/Blocks/BlocksMove.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Linq;
 
 public class BlocksMove : MonoBehaviour
 {
@@ -16,6 +17,7 @@ public class BlocksMove : MonoBehaviour
 
     private bool _lockRotation;
     private float _targetRotation;
+    private Bounds _boundingBox;
     //private Rigidbody2D _rigidbody;
 
     #endregion Fields
@@ -24,16 +26,17 @@ public class BlocksMove : MonoBehaviour
 
     //tant que le doigt bouge on vérifie le delta (direction) et on translate
 
-    //private void Start()
-    //{
-    //    _rigidbody = GetComponent<Rigidbody2D>();
-    //}
+    private void Start()
+    {
+        //_rigidbody = GetComponent<Rigidbody2D>();
+        SetBoundingBox();
+    }
 
     private void Update()
     {
         transform.Translate(_speed * Time.deltaTime * transform.InverseTransformDirection(-Vector3.up));
 
-        if ((int)transform.rotation.eulerAngles.z != _targetRotation % 360)
+        if ((int)transform.rotation.eulerAngles.z != _targetRotation % 360) //ATTENTION A LA SYNCHRO COLLISION UPDATE, légère différence à l'arrivé.
             transform.Rotate(0, 0, _rotationSpeed * 0.1f);
 
         if (Input.GetKeyDown(KeyCode.Return))
@@ -53,13 +56,17 @@ public class BlocksMove : MonoBehaviour
         if (collision.collider.tag == "Block")
         {
             FixeTransform();
+
+            //Il faut rechecker la collision après transform
             enabled = false;
+            //UTILISER LES CONTRAINTES AUSSI
         }
     }
 
     #endregion Unity Callbacks
 
-    //Utiliser raycast all plus tard
+    //Utiliser raycast all plus tard quand un piece s'arrete completement
+    //dans le meme event, checker si on est enfermé
 
     #region Private Methods
 
@@ -68,7 +75,30 @@ public class BlocksMove : MonoBehaviour
         int i = Mathf.RoundToInt(transform.rotation.eulerAngles.z / 90);
         transform.rotation = Quaternion.Euler(0, 0, i * 90);
 
-        transform.position = new Vector2(Mathf.RoundToInt(transform.position.x), transform.position.y);
+        //transform.position = new Vector2(Mathf.RoundToInt(transform.position.x), transform.position.y);
+        //en fonction de la taille et de la rotation, regarder si on arrondie sur 0.5 ou 1;
+        float horizontalSize = transform.rotation.z % 180 == 0 ? _boundingBox.size.x : _boundingBox.size.y;
+        if (horizontalSize % 2 == 0)
+        {
+            transform.position = new Vector2(Mathf.RoundToInt(transform.position.x), transform.position.y); //FACTORISER SUREMENT
+        }
+        else
+        {
+            //autre
+            //utiliser le modulo je pense
+        }
+    }
+
+    private void SetBoundingBox()
+    {
+        Transform[] childstransform = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+            childstransform[i] = transform.GetChild(i);
+
+        float extendX = childstransform.Max(tr => tr.localPosition.x + tr.localScale.x / 2);
+        float extendY = childstransform.Max(tr => tr.localPosition.y + tr.localScale.y / 2);
+        Vector3 size = new Vector3(extendX * 2, extendY * 2, 0);
+        _boundingBox = new Bounds(transform.position, size);
     }
 
     #endregion Private Methods
---
+++ Assets/Scripts/Blocks/PivotBlocksMove.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+public class PivotBlocksMove : MonoBehaviour
+{
+    #region Serialize Fields
+
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private int _rotationSpeed;
+
+    #endregion Serialize Fields
+
+    #region Fields
+
+    private bool _lockRotation;
+    private float _targetRotation;
+    private Transform _rotationTransform;
+
+    #endregion Fields
+
+    #region Unity Callbacks
+
+    //tant que le doigt bouge on vérifie le delta (direction) et on translate
+    private void Start()
+    {
+        _rotationTransform = transform.GetChild(0);
+    }
+
+    private void Update()
+    {
+        transform.Translate(_speed * Time.deltaTime * transform.InverseTransformDirection(-Vector3.up));
+
+        if ((int)_rotationTransform.rotation.eulerAngles.z != _targetRotation % 360)
+            _rotationTransform.Rotate(0, 0, _rotationSpeed * 0.1f);
+
+        if (Input.GetKeyDown(KeyCode.Return))
+            AddRotation();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Block")
+        {
+            FixeTransform();
+
+            //Il faut rechecker la collision après transform
+
+            enabled = false;
+            //UTILISER LES CONTRAINTES AUSSI
+        }
+    }
+
+    #endregion Unity Callbacks
+
+    //Utiliser raycast all plus tard quand un piece s'arrete completement
+    //dans le meme event, checker si on est enfermé
+
+    #region Private Methods
+
+    private void FixeTransform()
+    {
+        int i = Mathf.RoundToInt(_rotationTransform.rotation.eulerAngles.z / 90);
+        _rotationTransform.rotation = Quaternion.Euler(0, 0, i * 90);
+
+        transform.position = new Vector2(Mathf.RoundToInt(transform.position.x), transform.position.y);
+        //attention, ça dépend de la rotation et du nb de blocks horizontales
+        //Sinon faut jouer avec un pivot en plus
+    }
+
+    #endregion Private Methods
+
+    #region Public Methods
+
+    public void AddRotation()
+    {
+        _targetRotation += 90;
+    }
+
+    #endregion Public Methods
+}
